aa508be4a1f80830562b921bff9ae802e82ec5dc
0ldhands <iamkevinp11@gmail.com>
2025-04-01 08:15:41 -0700

    ii

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/kevinsportfolio/'
+  base:'/kevinsportfolio/',
+  build: {
+    outDir: 'dist', // Ensure the build output is in the 'dist' folder
+  },
 })
